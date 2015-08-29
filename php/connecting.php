865ad69600ee320d07b896d3d5b2f8f84dbb26bf
ElGatoSaez <geniointernet@gmail.com>
2015-08-29 00:14:00 -0300

    It works!
    Y rinde por litro

--- php/connecting.php
+++ php/connecting.php
@@ -10,15 +10,23 @@ Polsaker - https://github.com/Polsaker
  -
 */
 // Parameters
+https://hira.io/buttwait.php?tid=TRANSACTION_ID
 // Place here your buttcoin account (Putting mine as example)
 $acc = "elgatosaez";
 // Landing website (I'm putting mine as example)
-$cburl = "http://purr.cf/success";
+$cburl = "http://127.0.0.1/btc";
 // Amount you want to put, you can take this into another form if you want.
 $butts = "0";
 // Connecting to the API
 $api = "https://hira.io/butt.php?a=new&amount=" . $butts . "&callback=" . $cburl . "&deposit_to=" . $acc;
 // Retreiving those contents
 $getJson = file_get_contents($api);
-
+// Decoding that JSON to be readable
+$nowJson = json_decode($getJson, true);
+// Getting the Transaction ID
+$tID = $nowJson['id'];
+// Then getting the final webpage (dun dun dun dun)
+$fURL = "https://hira.io/buttwait.php?tid=" . $tID;
+// Redirecting to that
+header('Location: '.$fURL);
 ?>
